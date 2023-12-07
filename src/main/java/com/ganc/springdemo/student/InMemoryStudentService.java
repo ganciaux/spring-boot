da59d00d35b0d9ef829e77b994018abc94228d9a
ganciaux <ghislain.anciaux@live.fr>
2023-12-07 21:54:55 +0100

    Implementing dao layer

---
+++ src/main/java/com/ganc/springdemo/student/InMemoryStudentService.java
@@ -0,0 +1,40 @@
+package com.ganc.springdemo.student;
+
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+public class InMemoryStudentService implements StudentService{
+    private final InMemoryStudentDao dao;
+
+    public InMemoryStudentService(InMemoryStudentDao dao) {
+        this.dao = dao;
+    }
+
+    @Override
+    public Student save(Student s) {
+        return this.dao.save(s);
+    }
+
+    @Override
+    public List<Student> findAllStudents(){
+        return this.dao.findAllStudents();
+    }
+
+    @Override
+    public Student findByEmail(String email) {
+        return this.dao.findByEmail(email);
+    }
+
+    @Override
+    public Student update(Student s) {
+        return this.dao.update(s);
+    }
+
+    @Override
+    public void delete(String email) {
+        this.dao.delete(email);
+    }
+}
--- src/main/java/com/ganc/springdemo/student/StudentService.java
+++ src/main/java/com/ganc/springdemo/student/StudentService.java
@@ -5,12 +5,10 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.util.List;
 
-@Service
-public class StudentService {
-    public List<Student> findAllStudents(){
-        return List.of(
-                new Student("ganc","anc", LocalDate.now(),"ganc@anc",41),
-                new Student("ganc2","anc", LocalDate.now(),"ganc@anc",42)
-        );
-    }
+public interface StudentService {
+    Student save(Student s);
+    List<Student> findAllStudents();
+    Student findByEmail(String email);
+    Student update(Student s);
+    void delete(String email);
 }
